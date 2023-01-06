f59cdccc75fdbb601ea6eb2455caff8b13029852
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-05 22:19:31 -0600

    Add tests to Node.Test.cpp
    Added some test scenarios to Node.Test.cpp

--- unit-tests/src/Node/Node.Test.cpp
+++ unit-tests/src/Node/Node.Test.cpp
@@ -3,6 +3,10 @@
 #include "../../lib/Catch2/extras/catch_amalgamated.hpp"
 #include "../../../include/Node.hpp"
 
+/*
+ * This file contains the tests for the Node class
+ */
+
 SCENARIO("An empty node is created", "[node]") {
     GIVEN("An empty node") {
         // create an empty Node of type int
@@ -54,5 +58,61 @@ SCENARIO("An empty node is created", "[node]") {
                 REQUIRE(*(node->getValue()) == VALUES);
             }
         }
+
+        WHEN("A non-empty node is given to copy") {
+            // generate an new empty node that we will populate
+            shared_ptr<Node<int>> newNode(new Node<int>());
+            // empty node that comes after the current node
+            shared_ptr<Node<int>> nextNode(new Node<int>);
+            // empty node that comes before the current node
+            shared_ptr<Node<int>> prevNode(new Node<int>);
+            
+            // point newNode to nextNode
+            newNode->setNext(nextNode);
+            // point newNode to prevNode
+            newNode->setPrev(prevNode);
+
+            // generate values to use
+            const int VALUES = GENERATE(0, 1, 2, 5, 11, 21, 100, 1000);
+            // add generated values to value member
+            newNode->setValue(shared_ptr<int> (new int(VALUES)));
+
+            THEN("Copying the node should perform a deep copy") {
+                // this should invoke the copy constructor
+                node = newNode;
+                // we are checking that they are pointing to
+                // the same address
+                REQUIRE(node->getNext() == newNode->getNext());
+                // we are checking that they are pointing to
+                // the same address
+                REQUIRE(node->getPrev() == newNode->getPrev());
+                // here we want to make sure we are pointing to
+                // the same *value* (that the equals operator
+                // returns true for the two objects)
+                REQUIRE(*(node->getValue()) == *(newNode->getValue()));
+            }
+        }
     }
 }
+
+/*
+SCENARIO("A non-empty node is created", "[node]") {
+    GIVEN("A node with populated values") {
+        // node we will be working with
+        shared_ptr<Node<int>> node(new Node<int>);
+        // empty node that comes after the current node
+        shared_ptr<Node<int>> nextNode(new Node<int>);
+        // empty node that comes before the current node
+        shared_ptr<Node<int>> prevNode(new Node<int>);
+
+        // point the current node to the next node
+        node->setNext(nextNode);
+        // point the current node to the prev node
+        node->setPrev(prevNode);
+
+        // add a value to the current node
+        const int VALUES = GENERATE(0, 1, 2, 5, 11, 21, 100, 1000);
+        node->setValue(shared_ptr<int> (new int(VALUES)));
+    }
+}
+*/
